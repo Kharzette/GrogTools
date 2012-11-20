830655144ab1b0381995c2fba6494ae5c7743b37
Kharzette <6589537+Kharzette@users.noreply.github.com>
2012-11-20 18:15:23 +0000

    Ganked quite a lot of extra unneeded references and usings in the libs and tools

--- ColladaConvert/Forms/AnimGridModel.cs
+++ ColladaConvert/Forms/AnimGridModel.cs
@@ -4,10 +4,6 @@ using System.IO;
 using System.Xml;
 using System.ComponentModel;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Content;
-using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
-using Microsoft.Xna.Framework.Storage;
 using MeshLib;
 
 
