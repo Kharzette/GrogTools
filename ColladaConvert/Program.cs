ea26480edc80c61dbf739d6addc7859d3607227b
Kharzette <6589537+Kharzette@users.noreply.github.com>
2015-04-01 04:34:55 -0500

    Fix ColladaConvert, TestMeshes to work with the steering changes

--- ColladaConvert/Program.cs
+++ ColladaConvert/Program.cs
@@ -29,11 +29,12 @@ namespace ColladaConvert
 		{
 			MoveForwardBack, MoveForward, MoveBack,
 			MoveLeftRight, MoveLeft, MoveRight,
+			MoveForwardFast, MoveBackFast,
+			MoveLeftFast, MoveRightFast,
 			Turn, TurnLeft, TurnRight,
 			Pitch, PitchUp, PitchDown,
 			LightX, LightY, LightZ,
-			ToggleMouseLookOn, ToggleMouseLookOff,
-			BoostSpeedOn, BoostSpeedOff
+			ToggleMouseLookOn, ToggleMouseLookOff
 		};
 
 
@@ -142,14 +143,6 @@ namespace ColladaConvert
 							inp.UnMapAxisAction(MyActions.Pitch, Input.MoveAxis.MouseYAxis);
 							inp.UnMapAxisAction(MyActions.Turn, Input.MoveAxis.MouseXAxis);
 						}
-						else if(act.mAction.Equals(MyActions.BoostSpeedOn))
-						{
-							pSteering.Speed	=2;
-						}
-						else if(act.mAction.Equals(MyActions.BoostSpeedOff))
-						{
-							pSteering.Speed	=0.5f;
-						}
 					}
 				}
 
@@ -160,7 +153,7 @@ namespace ColladaConvert
 				
 				pos	=pSteering.Update(pos, gd.GCam.Forward, gd.GCam.Left, gd.GCam.Up, actions);
 				
-				gd.GCam.Update(pos, pSteering.Pitch, pSteering.Yaw, pSteering.Roll);
+				gd.GCam.Update(-pos, pSteering.Pitch, pSteering.Yaw, pSteering.Roll);
 
 				matLib.SetParameterForAll("mView", gd.GCam.View);
 				matLib.SetParameterForAll("mEyePos", gd.GCam.Position);
@@ -334,19 +327,28 @@ namespace ColladaConvert
 		{
 			Input	inp	=new InputLib.Input();
 			
+			inp.MapAction(MyActions.MoveForward, ActionTypes.ContinuousHold,
+				Modifiers.None, System.Windows.Forms.Keys.W);
+			inp.MapAction(MyActions.MoveLeft, ActionTypes.ContinuousHold,
+				Modifiers.None, System.Windows.Forms.Keys.A);
+			inp.MapAction(MyActions.MoveBack, ActionTypes.ContinuousHold,
+				Modifiers.None, System.Windows.Forms.Keys.S);
+			inp.MapAction(MyActions.MoveRight, ActionTypes.ContinuousHold,
+				Modifiers.None, System.Windows.Forms.Keys.D);
+			inp.MapAction(MyActions.MoveForwardFast, ActionTypes.ContinuousHold,
+				Modifiers.ShiftHeld, System.Windows.Forms.Keys.W);
+			inp.MapAction(MyActions.MoveBackFast, ActionTypes.ContinuousHold,
+				Modifiers.ShiftHeld, System.Windows.Forms.Keys.S);
+			inp.MapAction(MyActions.MoveLeftFast, ActionTypes.ContinuousHold,
+				Modifiers.ShiftHeld, System.Windows.Forms.Keys.A);
+			inp.MapAction(MyActions.MoveRightFast, ActionTypes.ContinuousHold,
+				Modifiers.ShiftHeld, System.Windows.Forms.Keys.D);
+
 			inp.MapAction(MyActions.PitchUp, ActionTypes.ContinuousHold, Modifiers.None, 16);
-			inp.MapAction(MyActions.MoveForward, ActionTypes.ContinuousHold, Modifiers.None, 17);
 			inp.MapAction(MyActions.PitchDown, ActionTypes.ContinuousHold, Modifiers.None, 18);
-			inp.MapAction(MyActions.MoveLeft, ActionTypes.ContinuousHold, Modifiers.None, 30);
-			inp.MapAction(MyActions.MoveBack, ActionTypes.ContinuousHold, Modifiers.None, 31);
-			inp.MapAction(MyActions.MoveRight, ActionTypes.ContinuousHold, Modifiers.None, 32);
 			inp.MapAction(MyActions.LightX, ActionTypes.ContinuousHold, Modifiers.None, 36);
 			inp.MapAction(MyActions.LightY, ActionTypes.ContinuousHold, Modifiers.None, 37);
 			inp.MapAction(MyActions.LightZ, ActionTypes.ContinuousHold, Modifiers.None, 38);
-
-			inp.MapToggleAction(MyActions.BoostSpeedOn,
-				MyActions.BoostSpeedOff, Modifiers.None,
-				42);
 
 			inp.MapToggleAction(MyActions.ToggleMouseLookOn,
 				MyActions.ToggleMouseLookOff, Modifiers.None,
@@ -369,14 +371,16 @@ namespace ColladaConvert
 			PlayerSteering	pSteering	=new PlayerSteering();
 			pSteering.Method			=PlayerSteering.SteeringMethod.Fly;
 
-			pSteering.SetMoveEnums(MyActions.MoveLeftRight, MyActions.MoveLeft, MyActions.MoveRight,
-				MyActions.MoveForwardBack, MyActions.MoveForward, MyActions.MoveBack);
+			pSteering.SetMoveEnums(MyActions.MoveForwardBack, MyActions.MoveLeftRight,
+				MyActions.MoveForward, MyActions.MoveBack, MyActions.MoveLeft,
+				MyActions.MoveRight, MyActions.MoveForwardFast, MyActions.MoveBackFast,
+				MyActions.MoveLeftFast, MyActions.MoveRightFast);
 
 			pSteering.SetTurnEnums(MyActions.Turn, MyActions.TurnLeft, MyActions.TurnRight);
 
 			pSteering.SetPitchEnums(MyActions.Pitch, MyActions.PitchUp, MyActions.PitchDown);
 
-			pSteering.Speed	=0.5f;
+			pSteering.Speed	=0.25f;
 
 			return	pSteering;
 		}
