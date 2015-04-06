ecfe86b218e1f2cb3a30f40650794329c2050c7d
Kharzette <6589537+Kharzette@users.noreply.github.com>
2015-04-06 15:24:47 -0500

    Fix bspbuilder with the playersteering and input changes

--- BSPBuilder/Program.cs
+++ BSPBuilder/Program.cs
@@ -29,12 +29,15 @@ namespace BSPBuilder
 		{
 			MoveForwardBack, MoveForward, MoveBack,
 			MoveLeftRight, MoveLeft, MoveRight,
+			MoveForwardFast, MoveBackFast,
+			MoveLeftFast, MoveRightFast,
 			Turn, TurnLeft, TurnRight,
 			Pitch, PitchUp, PitchDown,
 			LightX, LightY, LightZ,
-			ToggleMouseLookOn, ToggleMouseLookOff,
-			BoostSpeedOn, BoostSpeedOff
+			ToggleMouseLookOn, ToggleMouseLookOff
 		};
+
+		const float	MouseTurnMultiplier	=.07f;
 
 
 		[STAThread]
@@ -113,18 +116,10 @@ namespace BSPBuilder
 							inp.UnMapAxisAction(MyActions.Pitch, Input.MoveAxis.MouseYAxis);
 							inp.UnMapAxisAction(MyActions.Turn, Input.MoveAxis.MouseXAxis);
 						}
-						else if(act.mAction.Equals(MyActions.BoostSpeedOn))
-						{
-							pSteering.Speed	=3;
-						}
-						else if(act.mAction.Equals(MyActions.BoostSpeedOff))
-						{
-							pSteering.Speed	=0.5f;
-						}
 					}
 				}
 
-				pos	=pSteering.Update(pos, gd.GCam.Forward, gd.GCam.Left, gd.GCam.Up, actions);
+				pos	-=pSteering.Update(pos, gd.GCam.Forward, gd.GCam.Left, gd.GCam.Up, actions);
 				
 				gd.GCam.Update(pos, pSteering.Pitch, pSteering.Yaw, pSteering.Roll);
 
@@ -159,21 +154,28 @@ namespace BSPBuilder
 
 		static Input SetUpInput()
 		{
-			Input	inp	=new InputLib.Input();
+			Input	inp	=new InputLib.Input(MouseTurnMultiplier, 1000f / Stopwatch.Frequency);
 			
-			inp.MapAction(MyActions.PitchUp, ActionTypes.ContinuousHold, Modifiers.None, 16);
-			inp.MapAction(MyActions.MoveForward, ActionTypes.ContinuousHold, Modifiers.None, 17);
-			inp.MapAction(MyActions.PitchDown, ActionTypes.ContinuousHold, Modifiers.None, 18);
-			inp.MapAction(MyActions.MoveLeft, ActionTypes.ContinuousHold, Modifiers.None, 30);
-			inp.MapAction(MyActions.MoveBack, ActionTypes.ContinuousHold, Modifiers.None, 31);
-			inp.MapAction(MyActions.MoveRight, ActionTypes.ContinuousHold, Modifiers.None, 32);
+			inp.MapAction(MyActions.MoveForward, ActionTypes.ContinuousHold,
+				Modifiers.None, System.Windows.Forms.Keys.W);
+			inp.MapAction(MyActions.MoveLeft, ActionTypes.ContinuousHold,
+				Modifiers.None, System.Windows.Forms.Keys.A);
+			inp.MapAction(MyActions.MoveBack, ActionTypes.ContinuousHold,
+				Modifiers.None, System.Windows.Forms.Keys.S);
+			inp.MapAction(MyActions.MoveRight, ActionTypes.ContinuousHold,
+				Modifiers.None, System.Windows.Forms.Keys.D);
+			inp.MapAction(MyActions.MoveForwardFast, ActionTypes.ContinuousHold,
+				Modifiers.ShiftHeld, System.Windows.Forms.Keys.W);
+			inp.MapAction(MyActions.MoveBackFast, ActionTypes.ContinuousHold,
+				Modifiers.ShiftHeld, System.Windows.Forms.Keys.S);
+			inp.MapAction(MyActions.MoveLeftFast, ActionTypes.ContinuousHold,
+				Modifiers.ShiftHeld, System.Windows.Forms.Keys.A);
+			inp.MapAction(MyActions.MoveRightFast, ActionTypes.ContinuousHold,
+				Modifiers.ShiftHeld, System.Windows.Forms.Keys.D);
+
 			inp.MapAction(MyActions.LightX, ActionTypes.ContinuousHold, Modifiers.None, 36);
 			inp.MapAction(MyActions.LightY, ActionTypes.ContinuousHold, Modifiers.None, 37);
 			inp.MapAction(MyActions.LightZ, ActionTypes.ContinuousHold, Modifiers.None, 38);
-
-			inp.MapToggleAction(MyActions.BoostSpeedOn,
-				MyActions.BoostSpeedOff, Modifiers.None,
-				42);
 
 			inp.MapToggleAction(MyActions.ToggleMouseLookOn,
 				MyActions.ToggleMouseLookOff, Modifiers.None,
@@ -196,14 +198,15 @@ namespace BSPBuilder
 			PlayerSteering	pSteering	=new PlayerSteering();
 			pSteering.Method			=PlayerSteering.SteeringMethod.Fly;
 
-			pSteering.SetMoveEnums(MyActions.MoveLeftRight, MyActions.MoveLeft, MyActions.MoveRight,
-				MyActions.MoveForwardBack, MyActions.MoveForward, MyActions.MoveBack);
+			pSteering.SetMoveEnums(MyActions.MoveForwardBack, MyActions.MoveLeftRight,
+				MyActions.MoveForward, MyActions.MoveBack,
+				MyActions.MoveLeft, MyActions.MoveRight,
+				MyActions.MoveForwardFast, MyActions.MoveBackFast,
+				MyActions.MoveLeftFast, MyActions.MoveRightFast);
 
 			pSteering.SetTurnEnums(MyActions.Turn, MyActions.TurnLeft, MyActions.TurnRight);
 
 			pSteering.SetPitchEnums(MyActions.Pitch, MyActions.PitchUp, MyActions.PitchDown);
-
-			pSteering.Speed	=0.5f;
 
 			return	pSteering;
 		}
