9470d66969f8e3ed3486e991eada07ecf48f2910
Kharzette <6589537+Kharzette@users.noreply.github.com>
2019-04-29 16:43:36 -0500

    fix and add colladaconvert project

--- ColladaConvert/Properties/Settings.Designer.cs
+++ ColladaConvert/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace ColladaConvert.Properties {
+namespace ColladaConvert {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
@@ -20,6 +20,102 @@ namespace ColladaConvert.Properties {
         public static Settings Default {
             get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0, 0")]
+        public global::System.Drawing.Point MainWindowPos {
+            get {
+                return ((global::System.Drawing.Point)(this["MainWindowPos"]));
+            }
+            set {
+                this["MainWindowPos"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0, 0")]
+        public global::System.Drawing.Point AnimFormPos {
+            get {
+                return ((global::System.Drawing.Point)(this["AnimFormPos"]));
+            }
+            set {
+                this["AnimFormPos"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0, 0")]
+        public global::System.Drawing.Point MaterialFormPos {
+            get {
+                return ((global::System.Drawing.Point)(this["MaterialFormPos"]));
+            }
+            set {
+                this["MaterialFormPos"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0, 0")]
+        public global::System.Drawing.Point SkeletonEditorFormPos {
+            get {
+                return ((global::System.Drawing.Point)(this["SkeletonEditorFormPos"]));
+            }
+            set {
+                this["SkeletonEditorFormPos"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("100, 100")]
+        public global::System.Drawing.Size SkeletonEditorFormSize {
+            get {
+                return ((global::System.Drawing.Size)(this["SkeletonEditorFormSize"]));
+            }
+            set {
+                this["SkeletonEditorFormSize"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0, 0")]
+        public global::System.Drawing.Point CelTweakFormPos {
+            get {
+                return ((global::System.Drawing.Point)(this["CelTweakFormPos"]));
+            }
+            set {
+                this["CelTweakFormPos"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0, 0")]
+        public global::System.Drawing.Point SeamEditorFormPos {
+            get {
+                return ((global::System.Drawing.Point)(this["SeamEditorFormPos"]));
+            }
+            set {
+                this["SeamEditorFormPos"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("100, 100")]
+        public global::System.Drawing.Size SeamEditorFormSize {
+            get {
+                return ((global::System.Drawing.Size)(this["SeamEditorFormSize"]));
+            }
+            set {
+                this["SeamEditorFormSize"] = value;
             }
         }
     }
