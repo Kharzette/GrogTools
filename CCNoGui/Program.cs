a7a9c50dea78beb9b7bad520b818acf027f0b1e9
Kharzette <6589537+Kharzette@users.noreply.github.com>
2024-05-06 12:57:27 -0500

    Command line stuff from the other brnac

--- CCNoGui/Program.cs
+++ CCNoGui/Program.cs
@@ -9,5 +9,66 @@ internal static class Program
 {
 	static void Main(string []args)
 	{
+		if(args.Length == 0)
+		{
+			Console.WriteLine("Usage: -stuff");
+			return;
+		}
+
+		//default scale factor
+		MeshConverter.ScaleFactor	sf	=MeshConverter.ScaleFactor.Meters;
+
+		string	someArg	="";
+		bool	bStatic	=false;
+		bool	bChar	=false;
+		bool	bAnim	=false;
+
+		foreach(string arg in args)
+		{
+			if(arg == "-static")
+			{
+				bStatic	=true;
+			}
+			else if(arg == "-character")
+			{
+				bChar	=true;
+			}
+			else if(arg == "-anim")
+			{
+				bAnim	=true;
+			}
+			else if(arg == "-centimeters")
+			{
+				sf	=MeshConverter.ScaleFactor.Centimeters;
+			}
+			else if(arg == "-grog")
+			{
+				sf	=MeshConverter.ScaleFactor.Grog;
+			}
+			else if(arg == "-meters")
+			{
+				sf	=MeshConverter.ScaleFactor.Meters;
+			}
+			else if(arg == "-quake")
+			{
+				sf	=MeshConverter.ScaleFactor.Quake;
+			}
+			else if(arg == "-valve")
+			{
+				sf	=MeshConverter.ScaleFactor.Valve;
+			}
+			else
+			{
+				someArg	=arg;
+			}
+		}
+
+		if(bStatic)
+		{
+			if(!File.Exists(someArg))
+			{
+				Console.WriteLine("File: " + someArg + " not found.");
+			}
+		}
 	}
 }
